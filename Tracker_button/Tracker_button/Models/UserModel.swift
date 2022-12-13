b5278e4726539b2bb52fd10c8cd7e74913fae2ee
adrianasalomone <asalomone22@fed.idserve.net>
2022-12-13 23:51:52 +0100

    added showOnMap bool var, comment to explain in code

--- Tracker_button/Tracker_button/Models/UserModel.swift
+++ Tracker_button/Tracker_button/Models/UserModel.swift
@@ -15,6 +15,10 @@ struct User: Identifiable {
     let userLon: Double
     
     var userActivity: String
+    var showOnMap: Bool = false
+    
+        //to set it visible on map. Button with activity symbol on modal should be connected to userActivity and -when selected- change it to true to show on MapView. Probably a switch fallthrough running on enum every time you close modal to check all cases (activities) --> (to give the chanche to show more than one activity at a time on the map).
+    
     // var userActivityPin: Image           if needed --> uncomment import SwiftUI
     
     var userCoord: CLLocationCoordinate2D {
