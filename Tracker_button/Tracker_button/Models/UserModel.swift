cdcca3eca642fe4f42f3141184482b197a7b0502
adrianasalomone <asalomone22@fed.idserve.net>
2022-12-13 22:03:17 +0100

    Changed build version to iOs 16, create UserModel, changed id into var in RoutePoint.

---
+++ Tracker_button/Tracker_button/Models/UserModel.swift
@@ -0,0 +1,23 @@
+//
+//  UserModel.swift
+//  Tracker_button
+//
+//  Created by Adriana Salomone on 13/12/22.
+//
+
+import Foundation
+import MapKit
+// import SwiftUI
+
+struct User: Identifiable {
+    let id: UUID
+    let userLat: Double
+    let userLon: Double
+    
+    var userActivity: String
+    // var userActivityPin: Image           if needed --> uncomment import SwiftUI
+    
+    var userCoord: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: self.userLat, longitude: self.userLon)
+    }
+}
